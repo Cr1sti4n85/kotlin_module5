078e76d5da5f2b636f9443ba3c2b025e8726d04d
Cristian Perez <cperezlecaros@gmail.com>
2026-02-01 16:49:52 -0300

    Update CicloVidaActivity

--- app/src/main/java/com/example/m5kotlin/CicloVidaActivity.kt
+++ app/src/main/java/com/example/m5kotlin/CicloVidaActivity.kt
@@ -1,20 +1,86 @@
 package com.example.m5kotlin
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.m5kotlin.databinding.ActivityCicloVidaBinding
 
 class CicloVidaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCicloVidaBinding
+    private var eventCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_ciclo_vida)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityCicloVidaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        savedInstanceState?.let { bundle ->
+            val logsViejos = bundle.getString("SAVED_LOGS", "")
+            eventCount = bundle.getInt("SAVED_COUNT", 0)
+            binding.tvConsole.text = logsViejos
+            updateConsole("DATOS RESTAURADOS POST-ROTACIÓN")
         }
+
+        updateConsole("onCreate: Memoria asignada. La UI se está inflando.")
+
+        binding.btnClear.setOnClickListener {
+            binding.tvConsole.text = "Consola limpia."
+            eventCount = 0
+        }
+    }
+
+    private fun updateConsole(msg: String) {
+        eventCount++
+        val currentText = binding.tvConsole.text.toString()
+        val log = "\n[$eventCount] $msg"
+        binding.tvConsole.text = currentText + log
+
+        //bajar scroll hasta lo ultimo
+        binding.scrollView.post {
+            binding.scrollView.fullScroll(View.FOCUS_DOWN)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val textActual = binding.tvConsole.text.toString()
+        outState.putString("SAVED_LOGS", textActual)
+        outState.putInt("SAVED_COUNT", eventCount)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        updateConsole("onStart: La pantalla es visible.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateConsole("onResume: ¡LISTO! Puedes interactuar.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        updateConsole("onPause: Perdiendo el foco...")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        updateConsole("onStop: App oculta. Nos vamos a segundo plano.")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        updateConsole("onRestart: Volviendo de segundo plano.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        updateConsole("onDestroy: ¡DESTROOOY!")
+        Log.d("LIFECYCLE", "onDestroy: Memoria liberada.")
     }
 }
